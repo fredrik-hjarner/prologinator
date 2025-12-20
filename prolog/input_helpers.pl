4dbe19c5d94ba0a5fc910edb39914915cfac0d44
fredrik-hjarner <35084243+fredrik-hjarner@users.noreply.github.com>
2025-12-20 01:55:29 +0100

    cut about 20 lines of code

--- prolog/input_helpers.pl
+++ prolog/input_helpers.pl
@@ -11,26 +11,11 @@ key_down(KeyCode) -->
     ctx_events(Events),
     {member(event(key(KeyCode), down), Events)}.
 
-% Verbose alias
-key_pressed_this_frame(KeyCode) -->
-    key_down(KeyCode).
-
-key_down_this_frame(KeyCode) -->
-    key_down(KeyCode).
-
 % key_up(+KeyCode)
 % True if KeyCode had an 'up' event this frame
 key_up(KeyCode) -->
     ctx_events(Events),
     {member(event(key(KeyCode), up), Events)}.
-
-% Verbose alias
-key_released_this_frame(KeyCode) -->
-    key_up(KeyCode).
-
-% TODO: Not sure I like aliases. Also naming could be better
-key_up_this_frame(KeyCode) -->
-    key_up(KeyCode).
 
 % ==========================================================
 % Key State Queries (current state)
@@ -43,8 +28,4 @@ key_up_this_frame(KeyCode) -->
 key_held(KeyCode) -->
     ctx_held(Keys),
     {member(KeyCode, Keys)}.
-
-% Alias
-key_is_held(KeyCode) -->
-    key_held(KeyCode).
 
